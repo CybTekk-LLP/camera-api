02acdf0378556189d81201c81856f1f7884b6688
Shubhcyb2611 <s.gupta@cybtekk.com>
2024-06-24 15:16:52 +0530

    feat : multer upload controller added

--- src/interfaces/routers/index.ts
+++ src/interfaces/routers/index.ts
@@ -1,15 +1,8 @@
 import { Router } from "express";
-import { createRequire } from "node:module";
-const require = createRequire(import.meta.url);
-const swaggerFile = require("./../../swagger.output.json");
-
-import swaggerUi from "swagger-ui-express";
 import uploadRouter from "./upload.router";
 
 const appRouter = Router();
 
 appRouter.use("/uploads", uploadRouter);
-appRouter.use("/docs", swaggerUi.serve);
-appRouter.use("/docs", swaggerUi.setup(swaggerFile));
 
 export { appRouter };
