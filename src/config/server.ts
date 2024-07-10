a222e592d113c0b7899382bb7a4db574e6755c8a
Shubhcyb2611 <s.gupta@cybtekk.com>
2024-07-10 13:23:16 +0530

    feat: database coonnection added

--- src/config/server.ts
+++ src/config/server.ts
@@ -4,6 +4,7 @@ import { UPLOADS_PATH } from "./env.config";
 import { appRouter } from "@/interfaces/routers";
 import cors from "cors";
 import { corsConfig } from "@/interfaces/middleware";
+import { AppDataSource } from "@/infrastructure";
 
 export type AppConfig = {
   port?: number | string;
@@ -24,9 +25,20 @@ export class Server {
 
     this.app.use("/api", appRouter);
   }
+  private connectDatabase() {
+    AppDataSource.initialize()
+      .then(() => {
+        Logger.info("🤠: Database connection instantiated");
+      })
+      .catch((e) => {
+        Logger.error(e);
+        throw new Error("500::Failed to connect to database");
+      });
+  }
 
   start() {
     const port = this.config.port ?? 1209;
+    this.connectDatabase();
     this.app.listen(port, () => {
       Logger.info(`🚀: Server started on http://localhost:` + port);
     });
