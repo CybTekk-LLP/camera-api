2df8ef71ed6b53a9414e771645c5fc7b8fdcdf4f
Shubhcyb2611 <s.gupta@cybtekk.com>
2024-07-25 13:50:59 +0530

    fix: useless db integration

--- src/config/server.ts
+++ src/config/server.ts
@@ -4,9 +4,6 @@ import { UPLOADS_PATH } from "./env.config";
 import { appRouter } from "@/interfaces/routers";
 import cors from "cors";
 import { corsConfig } from "@/interfaces/middleware";
-import { AppDataSource } from "@/infrastructure";
-import { EmailService } from "@/application/services";
-import { startEmailCronJob } from "@/interfaces/utils";
 
 export type AppConfig = {
   port?: number | string;
@@ -26,28 +23,10 @@ export class Server {
     this.app.use("/api/uploads", express.static(UPLOADS_PATH));
 
     this.app.use("/api", appRouter);
-    const emailService = new EmailService();
-
-    // startEmailCronJob(
-    //   emailService,
-    //   "s.gupta@cybtekk.com",
-    //   "https://d3k81ch9hvuctc.cloudfront.net/company/TWnsJV/images/e9c9c693-0df8-4954-8b6d-47af088b0fd1.jpeg"
-    // );
-  }
-  private connectDatabase() {
-    AppDataSource.initialize()
-      .then(() => {
-        Logger.info("🤠: Database connection instantiated");
-      })
-      .catch((e) => {
-        Logger.error(e);
-        throw new Error("500::Failed to connect to database");
-      });
   }
 
   start() {
-    const port = this.config.port ?? 1209;
-    this.connectDatabase();
+    const port = this.config.port || 1200;
     this.app.listen(port, () => {
       Logger.info(`🚀: Server started on http://localhost:` + port);
     });
